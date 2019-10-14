66e6df83fc632cafd9a9df68acdb169e9d3f2bc3
AlexxNB <iam@alexxnb.ru>
2019-10-14 11:44:32 +0300

    fix single and empty tags

--- src/handlers/tags.js
+++ src/handlers/tags.js
@@ -4,15 +4,22 @@ export default function tags() {
 
     let marked = () => {};
 
-    const tags_replacer = (text,space1,open,tag,content,space2,close) => {
+    const singletags_replacer = (text,spaces,tag) => {
+        savedTags[id++] = tag;
+        return "##### svelte-md-tag-"+id+" #####";
+    }
 
-        content = before(content,marked);
-        content = content.replace(new RegExp(`^[\\t ]{0,${space2.length}}`, "gm"),'');
- 
-        content = marked(content);
-        if(!content.trim().match(/[\r\n]/g)) content = content.replace(/<p>|<\/p>/g,'').trim();
+    const tags_replacer = (text,space1,open,tag,content,space2,close) => {
+        
+        if(content.length > 0){
+            content = before(content,marked);
+            content = content.replace(new RegExp(`^[\\t ]{0,${space2.length}}`, "gm"),'');
+            content = marked(content);
+            if(!content.trim().match(/[\r\n]/g)) content = content.replace(/<p>|<\/p>/g,'').trim();
+        }
+        
         savedTags[id++] = `${open}${content}${close}`;
-        return " ##### svelte-md-tag-"+id+" ##### ";
+        return "##### svelte-md-tag-"+id+" #####";
     }
 
     const tags_restorator = (text,id) => {
@@ -22,8 +29,13 @@ export default function tags() {
     const before = (text,processor) => {
         marked = processor;
 
-        const re = /([\t ]*)(<([\w-:]+)[^>]*>)\n?(([\s]*)[\S\s]+?)(<\/\3>)/gm
-        if(text.match(re)) text = text.replace(re,tags_replacer);
+        const single_re = /([\t ]*)(<\w+[^>]*\/>)/g;
+        text = text.replace(single_re,singletags_replacer);
+        
+        const re = /([\t ]*)(<([\w-:]+)[^>]*>)\n?(([\s]*)[^<>]*?)(<\/\3>)/gm
+        while(text.match(re)) {
+            text = text.replace(re,tags_replacer);
+        }
 
         return text;
     }
@@ -35,5 +47,7 @@ export default function tags() {
         }
         return text;
     }
+
+
     return {before,after}
 }
---
+++ test/tests/test-11/source.md
@@ -0,0 +1,13 @@
+<!-- Test #11 -->
+<Example>
+    <div slot="text">
+        <Row>
+            <Col class="grid_col"> auto </Col>
+            <Col class="grid_col"> auto </Col>
+            <div class="is-full-width"></div>
+            <Col class="grid_col"> auto </Col>
+            <Col class="grid_col"> auto </Col>
+            <Col/>
+        </Row>
+    </div>
+</Example>
