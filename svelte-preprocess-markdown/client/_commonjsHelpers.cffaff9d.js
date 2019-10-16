5186d1f5ba74ccd38306a4a7c27507358061ea96
AlexxNB <AlexxNB@users.noreply.github.com>
2019-10-16 11:37:13 +0000

    Automated deployment: Wed Oct 16 11:37:13 UTC 2019 33c92a3c4de7f9e6ea34de9b4313fd4510154ba7

---
+++ svelte-preprocess-markdown/client/_commonjsHelpers.cffaff9d.js
@@ -0,0 +1,1 @@
+var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{};function o(e,o){return e(o={exports:{}},o.exports),o.exports}export{e as a,o as c};
