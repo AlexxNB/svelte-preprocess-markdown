1297e670a38eb429fef0a0b407bd29629a7af0f4
AlexxNB <AlexxNB@users.noreply.github.com>
2019-11-13 21:47:08 +0000

    Automated deployment: Wed Nov 13 21:47:08 UTC 2019 b7b35262b5adfb4388df37a7d8bda1920f2cc72e

---
+++ client/commonjsHelpers.7e9f9d63.js
@@ -0,0 +1,1 @@
+var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{};function o(){throw new Error("Dynamic requires are not currently supported by rollup-plugin-commonjs")}function n(e,o){return e(o={exports:{}},o.exports),o.exports}export{o as a,e as b,n as c};
